dffc5742ff4da41d73e59e0a962295a060ac1964
Karolina Gajdova <karolina.gajdova@etnetera.cz>
2020-03-14 17:16:00 +0100

    Add responsive

--- www/js/main.js
+++ www/js/main.js
@@ -1,3 +1,22 @@
 $(function(){
+    
+    $(document).on("click", ".burger-area", function(){
+       var list = $(this).parent().find("ul");
 
+       if(!list.hasClass("show")){
+           list.addClass("show");
+           $(this).addClass("burger-active")
+       } else {
+           list.removeClass("show");
+           $(this).removeClass("burger-active")
+       }
+    })
+
+    $(document).click(function(event) {
+        //if you click on anything except the modal itself or the "open modal" link, close the modal
+        if (!$(event.target).closest(".navigation").length) {
+            $(".navigation ul").removeClass("show");
+            $(".burger-area").removeClass("burger-active")
+        }
+    });
 });
