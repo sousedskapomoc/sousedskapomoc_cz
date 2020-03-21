0fb352963ac770271b7d07fed52949beb1c5145c
Pavel <admin@cermakpavel.com>
2020-03-21 16:40:50 +0100

    Add repositories

---
+++ app/Entities/Orders.php
@@ -0,0 +1,250 @@
+<?php
+
+
+namespace SousedskaPomoc\Entities;
+
+use SousedskaPomoc\Entities\Traits\Timestampable;
+use Doctrine\ORM\Mapping as ORM;
+use Nettrine\ORM\Entity\Attributes\Id;
+use SousedskaPomoc\Entities\Volunteer;
+use SousedskaPomoc\Entities\Address;
+
+
+/**
+ * Class Order
+ * @ORM\Entity(repositoryClass="SousedskaPomoc\Repository\OrderRepository")
+ * @ORM\HasLifecycleCallbacks()
+ */
+class Order
+{
+    use Id;
+
+//    use Timestampable;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Address", inversedBy="demandOrders")
+     * @ORM\Column(type="string")
+     */
+    protected $pickupAddress;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected $deliveryAddress;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Volunteer", inversedBy="createdOrders")
+     */
+    protected $author;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Volunteer", inversedBy="deliveredOrders")
+     */
+    protected $courier;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    protected $stat;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    protected $customerNote;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    protected $courierNote;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected $deliveryPhone;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    protected $items;
+
+
+
+    /**
+     * @return mixed
+     */
+    public function getItems()
+    {
+        return $this->items;
+    }
+
+
+
+    /**
+     * @param mixed $items
+     */
+    public function setItems($items) : void
+    {
+        $this->items = $items;
+    }
+
+
+
+
+    /**
+     * @return mixed
+     */
+    public function getDeliveryPhone()
+    {
+        return $this->deliveryPhone;
+    }
+
+
+
+    /**
+     * @param mixed $deliveryPhone
+     */
+    public function setDeliveryPhone($deliveryPhone) : void
+    {
+        $this->deliveryPhone = $deliveryPhone;
+    }
+
+
+
+    /**
+     * @return mixed
+     */
+    public function getCustomerNote()
+    {
+        return $this->customerNote;
+    }
+
+
+
+    /**
+     * @param mixed $customerNote
+     */
+    public function setCustomerNote($customerNote) : void
+    {
+        $this->customerNote = $customerNote;
+    }
+
+
+
+    /**
+     * @return mixed
+     */
+    public function getCourierNote()
+    {
+        return $this->courierNote;
+    }
+
+
+
+    /**
+     * @param mixed $courierNote
+     */
+    public function setCourierNote($courierNote) : void
+    {
+        $this->courierNote = $courierNote;
+    }
+
+
+
+    /**
+     * @return mixed
+     */
+    public function getPickupAddress()
+    {
+        return $this->pickupAddress;
+    }
+
+
+
+    /**
+     * @param mixed $pickupAddress
+     */
+    public function setPickupAddress($pickupAddress) : void
+    {
+        $this->pickupAddress = $pickupAddress;
+    }
+
+
+
+    /**
+     * @return mixed
+     */
+    public function getDeliveryAddress()
+    {
+        return $this->deliveryAddress;
+    }
+
+
+
+    /**
+     * @param mixed $deliveryAddress
+     */
+    public function setDeliveryAddress($deliveryAddress) : void
+    {
+        $this->deliveryAddress = $deliveryAddress;
+    }
+
+
+
+    /**
+     * @return mixed
+     */
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+
+
+    /**
+     * @param mixed $author
+     */
+    public function setAuthor($author) : void
+    {
+        $this->author = $author;
+    }
+
+
+
+    /**
+     * @return mixed
+     */
+    public function getCourier()
+    {
+        return $this->courier;
+    }
+
+
+
+    /**
+     * @param mixed $courier
+     */
+    public function setCourier($courier) : void
+    {
+        $this->courier = $courier;
+    }
+
+
+
+    /**
+     * @return mixed
+     */
+    public function getStatus()
+    {
+        return $this->stat;
+    }
+
+
+
+    /**
+     * @param mixed $status
+     */
+    public function setStatus($status) : void
+    {
+        $this->stat = $status;
+    }
+}
