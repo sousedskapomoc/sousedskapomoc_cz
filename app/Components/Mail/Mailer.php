bdabaebe97046aee433f35b31e06ee12bba0f93f
Martin Kucera <3159068+KuceraMartin@users.noreply.github.com>
2020-03-24 09:47:12 +0100

    use EmailManager in Mail

--- app/Components/Mail/Mailer.php
+++ app/Components/Mail/Mailer.php
@@ -3,9 +3,9 @@
 namespace SousedskaPomoc\Components;
 
 use Nette;
-use Nette\Mail\Mailer;
-use Nette\Mail\Message;
 use Latte\Engine;
+use SousedskaPomoc\Model\Email;
+use SousedskaPomoc\Model\EmailManager;
 
 
 final class Mail
@@ -13,108 +13,69 @@ final class Mail
     use Nette\SmartObject;
 
     /**
-     * @var Mailer
+     * @var EmailManager
      */
-    protected $mailer;
+    private $mailer;
 
 
 
-    public function __construct(Mailer $mailer)
+    public function __construct(EmailManager $mailer)
     {
         $this->mailer = $mailer;
     }
 
 
-
-    public function sendMail($to, $title, $body)
-    {
-        $mail = new Message;
-        $mail->setFrom('robot@sousedskapomoc.cz')
-            ->addTo($to)
-            ->setSubject($title)
-            ->setHtmlBody($body);
-
-        $this->mailer->send($mail);
-    }
-
-
-
     public function sendCourierMail($to, $link)
     {
-        $mail = new Message;
         $latte = new Engine;
-        $mail->setFrom('robot@sousedskapomoc.cz')
-            ->addTo($to)
-            ->setSubject('SousedskaPomoc.cz - Úspěšná registrace')
-            ->setHtmlBody($latte->renderToString(__DIR__.'/courierMail.latte', ['url' => $link]));
-
+        $body = $latte->renderToString(__DIR__.'/courierMail.latte', ['url' => $link]);
+		$mail = new Email($to, 'SousedskaPomoc.cz - Úspěšná registrace', $body);
         $this->mailer->send($mail);
     }
-
 
 
     public function sendSeamstressMail($to, $link)
     {
-        $mail = new Message;
         $latte = new Engine;
-        $mail->setFrom('robot@sousedskapomoc.cz')
-            ->addTo($to)
-            ->setSubject('SousedskaPomoc.cz - Úspěšná registrace')
-            ->setHtmlBody($latte->renderToString(__DIR__.'/seamstressMail.latte', ['url' => $link]));
+        $body = $latte->renderToString(__DIR__.'/seamstressMail.latte', ['url' => $link]);
+		$mail = new Email($to, 'SousedskaPomoc.cz - Úspěšná registrace', $body);
         $this->mailer->send($mail);
     }
-
 
 
     public function sendOperatorMail($to, $link)
     {
-        $mail = new Message;
         $latte = new Engine;
-        $mail->setFrom('robot@sousedskapomoc.cz')
-            ->addTo($to)
-            ->setSubject('SousedskaPomoc.cz - Úspěšná registrace')
-            ->setHtmlBody($latte->renderToString(__DIR__.'/operatorMail.latte', ['url' => $link]));
-
+		$body = $latte->renderToString(__DIR__.'/operatorMail.latte', ['url' => $link]);
+		$mail = new Email($to, 'SousedskaPomoc.cz - Úspěšná registrace', $body);
         $this->mailer->send($mail);
     }
-
 
 
     public function sendCoordinatorMail($to, $link)
     {
-        $mail = new Message;
         $latte = new Engine;
-        $mail->setFrom('robot@sousedskapomoc.cz')
-            ->addTo($to)
-            ->setSubject('SousedskaPomoc.cz - Úspěšná registrace')
-            ->setHtmlBody($latte->renderToString(__DIR__.'/coordinatorMail.latte', ['url' => $link]));
-
+		$body = $latte->renderToString(__DIR__ . '/coordinatorMail.latte', ['url' => $link]);
+		$mail = new Email($to, 'SousedskaPomoc.cz - Úspěšná registrace', $body);
         $this->mailer->send($mail);
     }
-
 
 
     public function sendLostPasswordMail($to, $link)
     {
-        $mail = new Message;
         $latte = new Engine;
-        $mail->setFrom('robot@sousedskapomoc.cz')
-            ->addTo($to)
-            ->setSubject('SousedskaPomoc.cz - Zapomenuté heslo')
-            ->setHtmlBody($latte->renderToString(__DIR__.'/lostPassword.latte', ['url' => $link]));
-
+		$body = $latte->renderToString(__DIR__.'/lostPassword.latte', ['url' => $link]);
+		$mail = new Email($to, 'SousedskaPomoc.cz - Zapomenuté heslo', $body);
         $this->mailer->send($mail);
     }
+
 
     public function sendSuperuserMail($to, $link)
     {
-        $mail = new Message;
         $latte = new Engine;
-        $mail->setFrom('robot@sousedskapomoc.cz')
-            ->addTo($to)
-            ->setSubject('SousedskaPomoc.cz - Úspěšná registrace')
-            ->setHtmlBody($latte->renderToString(__DIR__.'/superuserMail.latte', ['url' => $link]));
-
+        $body = $latte->renderToString(__DIR__.'/superuserMail.latte', ['url' => $link]);
+		$mail = new Email($to, 'SousedskaPomoc.cz - Úspěšná registrace', $body);
         $this->mailer->send($mail);
     }
+
 }
