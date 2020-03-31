8f002aa28844a55d3d5c60132d2222158b6118e4
Lukáš Huňka <lukas@hunka.cz>
2020-03-31 23:01:16 +0200

    Introduce Apolo Code Style

--- app/Bootstrap.php
+++ app/Bootstrap.php
@@ -6,11 +6,12 @@ namespace SousedskaPomoc;
 
 use Nette\Configurator;
 
+
 class Bootstrap
 {
-    public static function boot(): Configurator
-    {
-        $configurator = new Configurator;
+	public static function boot(): Configurator
+	{
+		$configurator = new Configurator;
 
         //$configurator->setDebugMode('23.75.345.200'); // enable for your remote IP
         //$configurator->setDebugMode(getenv('DEBUG') === 'on');
@@ -19,22 +20,22 @@ class Bootstrap
         $configurator->setTimeZone('Europe/Prague');
         $configurator->setTempDirectory(__DIR__ . '/../var/temp');
 
-        $configurator->createRobotLoader()
-            ->addDirectory(__DIR__)
-            ->register();
+		$configurator->createRobotLoader()
+			->addDirectory(__DIR__)
+			->register();
 
         $configurator
             ->addConfig(__DIR__ . '/../config/common.neon')
             ->addConfig(__DIR__ . '/../config/local.neon');
 
-        return $configurator;
-    }
+		return $configurator;
+	}
 
 
-    public static function bootForTests(): Configurator
-    {
-        $configurator = self::boot();
-        \Tester\Environment::setup();
-        return $configurator;
-    }
+	public static function bootForTests(): Configurator
+	{
+		$configurator = self::boot();
+		\Tester\Environment::setup();
+		return $configurator;
+	}
 }
