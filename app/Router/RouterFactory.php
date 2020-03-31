90a21325255a00aa2017dea03cd430890608f917
Lukáš Huňka <lukas@hunka.cz>
2020-03-31 23:01:16 +0200

    Introduce Apolo Code Style

--- app/Router/RouterFactory.php
+++ app/Router/RouterFactory.php
@@ -7,13 +7,12 @@ namespace SousedskaPomoc\Router;
 use Nette;
 use Nette\Application\Routers\RouteList;
 
-
 final class RouterFactory
 {
-	use Nette\StaticClass;
+    use Nette\StaticClass;
 
-	public static function createRouter(): RouteList
-	{
+    public static function createRouter(): RouteList
+    {
         $router = new RouteList;
         $router->addRoute('/change-password', 'Homepage:changePassword');
         $router->addRoute('[<locale=cs cs|en>/]<presenter>/<action>', 'Homepage:default');
