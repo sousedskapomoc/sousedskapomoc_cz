e4045287e85aa065bd0f4602b6e34cc9bbe4bbd7
Lukáš Huňka <lukas@hunka.cz>
2020-03-31 23:01:16 +0200

    Introduce Apolo Code Style

--- app/Bootstrap.php
+++ app/Bootstrap.php
@@ -15,18 +15,18 @@ class Bootstrap
 
 		//$configurator->setDebugMode('23.75.345.200'); // enable for your remote IP
 		$configurator->setDebugMode(getenv('DEBUG') === 'on');
-		$configurator->enableTracy(__DIR__ . '/../log');
+		$configurator->enableTracy(__DIR__ . '/../var/log');
 
 		$configurator->setTimeZone('Europe/Prague');
-		$configurator->setTempDirectory(__DIR__ . '/../temp');
+		$configurator->setTempDirectory(__DIR__ . '/../var/temp');
 
 		$configurator->createRobotLoader()
 			->addDirectory(__DIR__)
 			->register();
 
 		$configurator
-			->addConfig(__DIR__ . '/config/common.neon')
-			->addConfig(__DIR__ . '/config/local.neon');
+			->addConfig(__DIR__ . '/../config/common.neon')
+			->addConfig(__DIR__ . '/../config/local.neon');
 
 		return $configurator;
 	}
