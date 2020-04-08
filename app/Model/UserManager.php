8ff7ee9f26b5166d1745496a9d6fb7354518e9bc
Pavel <admin@cermakpavel.com>
2020-04-08 10:51:25 +0200

    Implement custom Authenticator

--- app/Model/UserManager.php
+++ app/Model/UserManager.php
@@ -11,7 +11,7 @@ use SousedskaPomoc\Repository\VolunteerRepository;
 /**
  * Users management.
  */
-final class UserManager implements Nette\Security\IAuthenticator
+final class UserManager
 {
     use Nette\SmartObject;
 
@@ -39,39 +39,6 @@ final class UserManager implements Nette\Security\IAuthenticator
         $this->database = $database;
         $this->passwords = $passwords;
         $this->volunteerRepository = $volunteerRepository;
-    }
-
-
-    /**
-     * Performs an authentication.
-     *
-     * @throws Nette\Security\AuthenticationException
-     */
-    public function authenticate(array $credentials): Nette\Security\IIdentity
-    {
-        [$username, $password] = $credentials;
-
-        $row = $this->database->table(self::TABLE_NAME)
-            ->where(self::COLUMN_NAME, $username)
-            ->fetch();
-
-        if (!$row) {
-            throw new Nette\Security\AuthenticationException('The username is incorrect.', self::IDENTITY_NOT_FOUND);
-        } elseif (!$this->passwords->verify($password, $row[self::COLUMN_PASSWORD_HASH])) {
-            throw new Nette\Security\AuthenticationException('The password is incorrect.', self::INVALID_CREDENTIAL);
-        } elseif ($this->passwords->needsRehash($row[self::COLUMN_PASSWORD_HASH])) {
-            $row->update([
-                self::COLUMN_PASSWORD_HASH => $this->passwords->hash($password),
-            ]);
-        }
-
-        $roles = explode(";", $row->role ?? 'user');
-
-        $arr = $row->toArray();
-        unset($arr[self::COLUMN_PASSWORD_HASH]);
-
-
-        return new Nette\Security\Identity($row[self::COLUMN_ID], $roles, $arr);
     }
 
 
