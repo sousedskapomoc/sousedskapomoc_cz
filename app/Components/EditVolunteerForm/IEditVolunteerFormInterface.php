8f2d3d0fd6eb567648ebf745b3052a8a1a3b1070
Pavel <admin@cermakpavel.com>
2020-04-08 09:29:45 +0200

    Move editVolunteerForm to commponent and modify it to use Doctrine

---
+++ app/Components/EditVolunteerForm/IEditVolunteerFormInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace SousedskaPomoc\Components;
+
+use SousedskaPomoc\Components\EditVolunteerForm;
+
+interface IEditVolunteerFormInterface
+{
+    /** @return EditVolunteerForm */
+    public function create();
+}
