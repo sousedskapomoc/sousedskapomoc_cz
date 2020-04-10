9e2da241d7ea61d6f0c6c08c5e5572836e92758f
Pavel Čermák <admin@cermakpavel.com>
2020-04-10 13:41:11 +0200

    Bugfix - seamstress add order

--- app/Presenters/SeamstressPresenter.php
+++ app/Presenters/SeamstressPresenter.php
@@ -6,12 +6,23 @@ namespace SousedskaPomoc\Presenters;
 
 use Contributte\FormsBootstrap\BootstrapForm;
 use Contributte\FormsBootstrap\Enums\RenderMode;
+use Nette\Security\AuthenticationException;
+use SousedskaPomoc\Entities\Address;
+use SousedskaPomoc\Entities\Order;
 use SousedskaPomoc\Model\OrderManager;
+use SousedskaPomoc\Repository\AddressRepository;
+use SousedskaPomoc\Repository\VolunteerRepository;
 
 final class SeamstressPresenter extends BasePresenter
 {
     /** @var OrderManager */
     protected $orderManager;
+
+    /** @var \SousedskaPomoc\Repository\AddressRepository */
+    protected $addressRepository;
+
+    /** @var \SousedskaPomoc\Repository\VolunteerRepository */
+    protected $volunteerRepository;
 
     public function beforeRender()
     {
@@ -27,39 +38,80 @@ final class SeamstressPresenter extends BasePresenter
         $this->orderManager = $orderManager;
     }
 
+    public function injectAddressRepository(AddressRepository $addressRepository) {
+        $this->addressRepository = $addressRepository;
+    }
+
+    public function injectVolunteerRepository(VolunteerRepository $volunteerRepository) {
+        $this->volunteerRepository = $volunteerRepository;
+    }
+
 
     public function createComponentPostOrder()
     {
-        $form = new BootstrapForm();
+        $form = new BootstrapForm;
 
-        $form->addText('delivery_address', $this->translator->translate('forms.postOrder.addressPick'))
-            ->setRequired($this->translator->translate('forms.postOrder.addressRequired'))
-            ->setPlaceholder($this->translator->translate('forms.postOrder.addressPlaceholder'));
+        $form->addText('pickupAddress', 'Adresa vyzvednuti')
+            ->setPlaceholder('Ulice včetně čísla popisného');
+        $form->addText('items', "Pocet rousek")
+            ->setRequired('Prosim sdelte nam kolik rousek je pripraveno k vyzvednuti.');
+        $form->addHidden('pickupId');
 
-        $form->addHidden('note')->setDefaultValue('rousky');
-
-        $form->addText('order_items', $this->translator->translate('templates.seamstress.itemsLabel'))
-            ->setPlaceholder('min. 10 kusů')
-            ->setRequired('Zadejte prosím počet roušek k vyzvednutí');
-
-        $form->addSubmit('postOrderFormSubmit', $this->translator->translate('templates.seamstress.button'));
-        $form->onSuccess[] = [$this, "postOrder"];
+        $form->addSubmit('addOrderFormSubmit', 'Odeslat poptavku');
+        $form->onSuccess[] = [$this, "processAdd"];
 
         return $form;
     }
 
 
-    public function postOrder(BootstrapForm $form)
+    public function processAdd(BootstrapForm $form)
     {
         $values = $form->getValues();
 
-        $values->id_volunteers = $this->user->getId();
-        $values->delivery_phone = $this->user->getIdentity()->data['personPhone'] ?? 'neuveden';
-        $values->status = "new";
+        /** @var Order $order */
+        $order = new Order();
+        $order->setItems($values->items);
+        $order->setStatus('new');
+        $order->setCourier(null);
+        /** @var \SousedskaPomoc\Entities\Volunteer $user */
+        $user = $this->volunteerRepository->getById($this->presenter->user->getId());
+        $user->addCreatedOrder($order);
+        $order->setOwner($user);
 
-        $result = $this->orderManager->create($values);
-        $this->flashMessage($this->translator->translate('messages.order.orderSuccess'));
-        $this->redirect("Coordinator:dashboard");
+        $client = new \GuzzleHttp\Client();
+        /** @var \GuzzleHttp\Psr7\Response $response */
+        $baseUri = "https://geocoder.ls.hereapi.com/6.2/geocode.json?locationid=";
+        $apiKey = "Kl0wK4fx38Pf63EIey6WyrmGEhS2IqaVHkuzx0IQ4-Q";
+        $pickupResponse = $client->get($baseUri . $values->pickupId . '&jsonattributes=1&gen=9&apiKey=' . $apiKey);
+        $pickupContent = $pickupResponse->getBody()->getContents();
+        $pickupContent = json_decode($pickupContent);
+        //Address information
+        $addrPickup = $pickupContent->response->view['0']->result['0']->location->address;
+        //HERE maps Id
+        $pickupId = $pickupContent->response->view['0']->result['0']->location->locationId;
+        //array with latitude and longtitude
+        $pickupGps = $pickupContent->response->view['0']->result['0']->location->displayPosition;
+
+        /** @var Address $pickupAddress */
+        $pickupAddress = new Address();
+        $pickupAddress->setCity($addrPickup->city);
+        $pickupAddress->setState($addrPickup->state);
+        $pickupAddress->setLocationId($pickupId);
+        $pickupAddress->setCountry($addrPickup->country);
+        $pickupAddress->setDistrict($addrPickup->county);
+        $pickupAddress->setPostalCode($addrPickup->postalCode);
+        $pickupAddress->setLongitude($pickupGps->longitude);
+        $pickupAddress->setLatitude($pickupGps->latitude);
+        $pickupAddress->addPickupOrder($order);
+
+
+        try {
+            $this->addressRepository->create($pickupAddress);
+            $this->flashMessage($this->translator->translate('messages.order.orderSuccess'));
+            $this->redirect("Coordinator:dashboard");
+        } catch (AuthenticationException $e) {
+            $form->addError($e->getMessage());
+        }
     }
 
 
