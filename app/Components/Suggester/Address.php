cfc7c3673c31605c747bd2278bcc42df81665329
Lukáš Huňka <lukas@hunka.cz>
2020-04-13 15:52:42 +0200

    Add FullAddress suggesting

---
+++ app/Components/Suggester/Address.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace SousedskaPomoc\Components\Suggester;
+
+use Nette\Application\UI\Control;
+
+class Address extends Control
+{
+    public function render() {
+        $this->template->setFile(__DIR__ . '/address.latte');
+        $this->template->render();
+    }
+}
---
+++ app/Components/Suggester/ISuggesterAddressInterface.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace SousedskaPomoc\Components\Suggester;
+
+interface ISuggesterAddressInterface
+{
+    /** @return Address */
+    public function create();
+}
